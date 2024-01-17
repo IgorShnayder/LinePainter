0b9c52cd1ef689029da2c15227e9878be99704dc
Igor Shnayder <shnayder.ig@gmail.com>
2024-01-17 10:18:40 +0100

    Initial commit

---
+++ Assets/Scripts/PaintingController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+ 
+public class PaintingController : MonoBehaviour
+{
+    [SerializeField] private TrailRenderer _trailRenderer;
+    [SerializeField] private Camera _camera;
+    
+    private void Awake()
+    {
+        _trailRenderer = GetComponent<TrailRenderer>();
+        _trailRenderer.emitting = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _trailRenderer.Clear();
+            _trailRenderer.emitting = true;
+            MovePainter();
+        }
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            _trailRenderer.emitting = false;
+        }
+        
+        MovePainter();
+    }
+
+    private void MovePainter()
+    {
+        var mousePosition = Input.mousePosition;
+        
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        mousePosition.x = Mathf.Clamp(mousePosition.x, 0f, screenWidth);
+        mousePosition.y = Mathf.Clamp(mousePosition.y, 0f, screenHeight);
+        
+        Cursor.lockState = CursorLockMode.Confined;
+        
+        var position = _camera.ScreenToWorldPoint(mousePosition);
+        gameObject.transform.position = new Vector3(position.x, position.y, 0);
+    }
+}
